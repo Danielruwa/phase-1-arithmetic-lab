122419b85d05edf20b6df082f122228eb540b2d1
Danielruwa <ruwa.dan7@gmail.com>
2024-03-25 11:51:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+//random number
+
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+
+// const number = Math.random();
+
+// const random = Math.random();
+// console.log(random)
+
+//mod
+
+var num3 = 20; 
+var num4 = 8; 
+
+var mod = (num3 % num4) === 4 ? 4 : (num3 % num4); 
+
+console.log(mod); 
+
+//max
+
+var set = [10, 5, 20, 15]; 
+var max = Math.max(...set); 
+
+console.log(max); 
+
+
+
